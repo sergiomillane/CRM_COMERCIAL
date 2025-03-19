9dd3b9d095753d5ffc2df4a6fa7e6c8888d1f11e
Sergio Millan <sergiomillane23@gmail.com>
2025-03-19 10:54:03 -0700

    Mods2.5.1

--- bitacora_credito/bitacora_credito.py
+++ bitacora_credito/bitacora_credito.py
@@ -40,19 +40,19 @@ with st.form("registro_form", clear_on_submit=True):
 
     with col1:
         fecha = st.date_input("Fecha", datetime.today())
-        ticket = st.text_area("Ticket")
+        ticket = st.text_area("Ticket", height=30)  # Altura reducida
         sucursal = st.selectbox("Sucursal", list(range(1, 101)))
         tipo_cliente = st.selectbox("Tipo de Cliente", ["RECOMPRA ACTIVO", "NUEVO", "RECOMPRA INACTIVO", "CAMPAÑA"])
         
     with col2:
         venta = st.selectbox("Venta", ["AUTORIZADA", "NO AUTORIZADA", "AUTORIZADA PARCIAL"])
-        cliente = st.text_area("ID_Cliente", help="Presiona `Ctrl + Enter` para enviar")
-        enganche_requerido = st.number_input("Enganche Requerido", min_value=0.0, format="%.2f")
+        cliente = st.text_area("ID_Cliente",height=30)
         notas = st.selectbox("Notas", ["CON ENGANCHE", "SIN ENGANCHE", "OTRO"])
+        enganche_requerido = st.number_input("Enganche Requerido", min_value=0.0, format="%.2f")
 
     with col3:
         moto = st.selectbox("Moto", ["SI", "NO"])
-        observacion = st.text_area("Observación", help="Presiona `Ctrl + Enter` para enviar")
+        observacion = st.text_area("Observación", height=30)
         lc_actual = st.number_input("LC Actual", min_value=0.0, format="%.2f")
         lc_final = st.number_input("LC Final", min_value=0.0, format="%.2f")
         
